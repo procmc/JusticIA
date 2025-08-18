ebab61ccf93722b298c446fb272cb25ef7cf34b7
Yeslin Chinchilla Ruiz <YESLIN.CHINCHILLA@ucr.ac.cr>
2025-08-18 16:35:50 -0600

    probando

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
         }}
       >
         <h1 className="text-2xl md:text-3xl font-extrabold text-azulOscuro text-center drop-shadow-lg tracking-tight animate-fade-in-scale">
-          ¡Holaaaaa!
+          ¡Holaaaaa buenaas!
         </h1>
       </div>
     </div>
