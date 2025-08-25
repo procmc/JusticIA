9604c7ee67999deceba16171448b81bfc1db6129
Yeslin Chinchilla Ruiz <YESLIN.CHINCHILLA@ucr.ac.cr>
2025-08-25 17:21:23 -0600

    Home listo

--- frontend/components/auth/login/Login.jsx
+++ frontend/components/auth/login/Login.jsx
@@ -4,7 +4,6 @@ import { useRouter } from "next/router";
 import { loginService } from "../../../services/authService";
 import LockAnimationSystem from "./LockAnimationSystem";
 import LoginForm from "./LoginForm";
-import { useLoginAnimations } from "../../../hooks/useLoginAnimations";
 import { signIn } from "next-auth/react";
 
 const Login = () => {
@@ -12,12 +11,6 @@ const Login = () => {
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState(null);
   const router = useRouter();
-  const { 
-    isUnlocked, 
-    showSuccess, 
-    isClosing, 
-    triggerSuccessAnimation 
-  } = useLoginAnimations();
   const toggleVisibility = () => setIsVisible(!isVisible);
 
   const handleLogin = async (e) => {
@@ -55,9 +48,9 @@ const Login = () => {
       <Card className="flex flex-col md:flex-row w-full max-w-lg md:max-w-4xl lg:max-w-5xl h-auto md:min-h-[600px] shadow-lg pb-8 md:pb-0" shadow="lg" radius="lg">
         {/* Sección izquierda con animaciones del candado */}
         <LockAnimationSystem 
-          isUnlocked={isUnlocked}
-          showSuccess={showSuccess}
-          isClosing={isClosing}
+          isUnlocked={true}
+          showSuccess={false}
+          isClosing={true}
         />
 
         {/* Sección derecha con el formulario */}
